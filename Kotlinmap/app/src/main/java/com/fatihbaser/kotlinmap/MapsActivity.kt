67fc21020d4329b3c5d003acaa6f92cfe2e4d3e7
fatih40 <baserfatih40@gmail.com>
2021-04-03 12:29:21 +0200

    is finished

--- Kotlinmap/app/src/main/java/com/fatihbaser/kotlinmap/MapsActivity.kt
+++ Kotlinmap/app/src/main/java/com/fatihbaser/kotlinmap/MapsActivity.kt
@@ -1,7 +1,16 @@
 package com.fatihbaser.kotlinmap
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,11 +18,15 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.lang.Exception
+import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationListener: LocationListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
@@ -23,21 +36,73 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+      /*  // Add a marker in Sydney and move the camera
+        val sydney = LatLng(37.75974383270371, 30.567072343433285)
+        mMap.addMarker(MarkerOptions().position(sydney).title("ISPARTAAA"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,15F))*/
+
+        locationManager =getSystemService(Context.LOCATION_SERVICE)as LocationManager
+        locationListener =object : LocationListener{
+            override fun onLocationChanged(location: Location) {
+
+
+                if (location !=null){
+                    mMap.clear()
+                    val userLocation=LatLng(location.latitude,location.longitude)
+                    mMap.addMarker(MarkerOptions().position(userLocation).title("Your Location"))
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,15f))
+                    val geocoder= Geocoder(this@MapsActivity, Locale.getDefault())
+
+
+
+                    try {
+                        val adressList =geocoder.getFromLocation(location.latitude,location.longitude,1)
+
+                        if(adressList != null && adressList.size>0){
+                            println(adressList.get(0).toString())
+
+                        }
+                    }catch (e:Exception){
+                        println("olmuyor")
+                        e.printStackTrace()
+                    }
+                }
+            }
+
+        }
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),1)
+        }else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1,1f,locationListener)
+
+            val lastLocation=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if(lastLocation != null){
+                val lasKnoenLatlng=LatLng(lastLocation.latitude,lastLocation.longitude)
+                mMap.addMarker(MarkerOptions().position(lasKnoenLatlng).title("Your Location"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lasKnoenLatlng,15f))
+
+
+            }
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode==1){
+            if(grantResults.size>0){
+                if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1,1f,locationListener)
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
